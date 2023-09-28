0e0dd4dda73f82cd1a40bb2e15184f6edbff89a8
haiiro2gou <mrashen.2gou@gmail.com>
2023-09-28 22:53:11 +0900

    server ping system

--- src/index.js
+++ src/index.js
@@ -12,22 +12,42 @@ import getServerStatus from './utils/getServerStatus.js';
 
 cron.schedule('*/15 * * * * *', async () => {
     console.log('Started pinging to the server!');
+    var result = [true, true, true, true];
 
     // proxy (ngrok)
     try {
-        const response = await got({ url: 'http://127.0.0.1:4040/api/tunnels/' });
-        const proxyAddress = (JSON.parse(response.body).tunnels[0].public_url).substring(6).split(':', 2);
-        console.log(`${proxyAddress[0]}`);
-        getServerStatus(proxyAddress[0], proxyAddress[1]);
+        const proxyResponse = await got({ url: 'http://127.0.0.1:4040/api/tunnels/' });
+        const proxyParse = (JSON.parse(proxyResponse.body).tunnels[0].public_url).substring(6).split(':', 2);
+        const proxyAddress = proxyParse[0], proxyPort = Number(proxyParse[1]);
+        result[0] = getServerStatus(proxyAddress, proxyPort);
     } catch (error) {
         console.log(`Error has occured during getting response from the proxy: ${error}`);
     }
 
     // hub
-
+    try {
+        const hubAddress = '127.0.0.1', hubPort = process.env.SERV_HUB;
+        result[1] = getServerStatus(hubAddress, hubPort);
+    } catch (error) {
+        console.log(`Error has occured during getting response from the hub server: ${error}`);
+    }
     // temporary
-
+    try {
+        const tempAddress = '192.168.2.121', tempPort = process.env.SERV_TEMP;
+        result[2] = getServerStatus(tempAddress, tempPort);
+    } catch (error) {
+        console.log(`Error has occured during getting response from the temporary server: ${error}`);
+    }
     // event
+    try {
+        const eventAddress = '192.168.2.121', eventPort = process.env.SERV_EVENT;
+        result[3] = getServerStatus(eventAddress, eventPort);
+    } catch (error) {
+        console.log(`Error has occured during getting response from the event server: ${error}`);
+    }
+
+    // modify message
+
 });
 
 client.login(process.env.DISCORD_TOKEN);
--- src/utils/getServerStatus.js
+++ src/utils/getServerStatus.js
@@ -1,14 +1,17 @@
 import got from 'got';
 import util from 'minecraft-server-util';
 const options = {
-    timeout: 1000 * 5,
+    timeout: 1000 * 2,
     enableSRV: true
 };
 
 export default async (addr, port) => {
+    var check = true;
     try {
         const result = await util.status(addr, port, options);
     } catch (error) {
         console.log(`Error has occurred during getting server status: ${error}`);
+        check = false;
     }
+    return check;
 };
