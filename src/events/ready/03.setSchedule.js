030ed4c440008625e41ec8d8e00aef37a5314e85
haiiro2gou <mrashen.2gou@gmail.com>
2023-10-01 21:41:05 +0900

    updated submodule

--- src/events/ready/03.setSchedule.js
+++ src/events/ready/03.setSchedule.js
@@ -20,19 +20,19 @@ export default async (client) => {
             const ngrokParse = (JSON.parse(ngrokResponse.body).tunnels[0].public_url).substring(6).split(':', 2);
             const ngrokAddress = ngrokParse[0], ngrokPort = Number(ngrokParse[1]);
             result[0] = await getServerStatus(ngrokAddress, ngrokPort);
-        } catch (error) { result[0] = { check: false, data: {} }; }
+        } catch (error) { result[0] = { check: false }; }
         try {
             const hubAddress = process.env.SELF_IP, hubPort = Number(process.env.PORT_HUB);
             result[2] = await getServerStatus(hubAddress, hubPort);
-        } catch (error) { result[2] = { check: false, data: {} }; }
+        } catch (error) { result[2] = { check: false }; }
         try {
             const tempAddress = process.env.HUB_IP, tempPort = Number(process.env.PORT_TEMP);
             result[3] = await getServerStatus(tempAddress, tempPort);
-        } catch (error) { result[3] = { check: false, data: {} }; }
+        } catch (error) { result[3] = { check: false }; }
         try {
             const eventAddress = process.env.HUB_IP, eventPort = Number(process.env.PORT_EVENT);
             result[4] = await getServerStatus(eventAddress, eventPort);
-        } catch (error) { result[4] = { check: false, data: {} }; }
+        } catch (error) { result[4] = { check: false }; }
     
         modifyMessages(client, result);
     }, 60000);
--- src/utils/modifyMessages.js
+++ src/utils/modifyMessages.js
@@ -6,9 +6,9 @@ export default async (client, input) => {
 
         const title = '## Server Status\n';
         const ngrok = '```' + (input[0].check ? `${input[0].data.ip}` : 'N/A') + '```\n';
-        const hub = (input[2].check ? `:green_circle: [Hub] - (${input[2].data.version.name})\n` : `:red_circle: [Hub] -\n`);
-        const temp = (input[3].check ? `:green_circle: [Temp] ${input[3].data.motd.clean} (${input[3].data.version.name})\n` : `:red_circle: [Temp] -\n`);
-        const event = (input[4].check ? `:green_circle: [Event] ${input[4].data.motd.clean} (${input[4].data.version.name})\n` : `:red_circle: [Event] -\n`);
+        const hub = (input[2].check ? `:green_circle: [Hub] - (${input[2].data?.version.name})\n` : `:red_circle: [Hub] -\n`);
+        const temp = (input[3].check ? `:green_circle: [Temp] ${input[3].data?.motd.clean} (${input[3].data?.version.name})\n` : `:red_circle: [Temp] -\n`);
+        const event = (input[4].check ? `:green_circle: [Event] ${input[4].data?.motd.clean} (${input[4].data?.version.name})\n` : `:red_circle: [Event] -\n`);
         const time = `\nLast Update: ${new Date()} `;
 
         const messages = await channel.messages.fetch({ limit: 50 });
