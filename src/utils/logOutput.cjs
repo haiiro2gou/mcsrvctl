ea94c49e1b4144df12d419d8c1b153cd8a2a816a
はいいろ <mrashen.2gou@gmail.com>
2024-07-31 21:32:27 +0900

    :technologist: reworked log system

--- src/utils/logOutput.cjs
+++ src/utils/logOutput.cjs
@@ -1,5 +1,9 @@
-module.exports = (text, attribute = "Info") => {
+module.exports = (message, attribute = "Info") => {
     const date = new Date();
     const hh = ('00' + date.getHours()).slice(-2), mm = ('00' + date.getMinutes()).slice(-2), ss = ('00' + date.getSeconds()).slice(-2);
-    console.log(`[${hh}:${mm}:${ss}] [${attribute}] ${text}`);
+    if ((typeof message) !== "string") message = message.toString();
+    const messages = message.split('\n');
+    for (row of messages) {
+        console.log(`[${hh}:${mm}:${ss}] [${attribute}] ${row}`);
+    }
 }
