d79da9d65ce24dd2ede699be95c0b35e9f31041d
はいいろ <mrashen.2gou@gmail.com>
2024-08-03 02:46:28 +0900

    :adhesive_bandage: fixed ping

--- src/utils/getServerStatus.js
+++ src/utils/getServerStatus.js
@@ -1,7 +1,7 @@
 import { MinecraftServerListPing } from 'minecraft-status';
 
 export default async (addr, port = 25565, query = false) => {
-    MinecraftServerListPing.ping(4, addr, port, 5000)
+    await MinecraftServerListPing.ping(4, addr, port, 5000)
     .then((response) => {
         return {
             online: true,
